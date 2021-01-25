bd8e56154341a90126df324b95787c334e610e34
Roman Strakhov <rvstrakhov@gmail.com>
2021-01-25 21:06:41 +0300

    update engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -12,8 +12,8 @@ formats = {
 def generate_diff(first_file, second_file, _format='stylish'):
 
     try:  # noqa: WPS229
-        previous_file_extension, previous_file = read_file(first_file)
-        current_file_extension, current_file = read_file(second_file)
+        previous_file = read_file(first_file)
+        current_file = read_file(second_file)
 
     except FileNotFoundError as error:
         print(str(error)[10:])
@@ -22,11 +22,5 @@ def generate_diff(first_file, second_file, _format='stylish'):
         print('Sorry, {0} file type is not supported.'.format(error))
 
     else:
-
-        if previous_file_extension == current_file_extension:
-            diff = get_difference(previous_file, current_file)
-            print(formats[_format](diff))
-        else:
-            print(
-                'Sorry, but files must be of the same type.',
-            )
+        diff = get_difference(previous_file, current_file)
+        return formats[_format](diff)
