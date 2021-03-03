81f9d6ae2eb1966bc779f639f1668d0f931f8ef8
Roman Strakhov <rvstrakhov@gmail.com>
2021-03-03 14:17:45 +0300

    rename get_difference arguments

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -12,31 +12,31 @@ UNCHANGED = 'unchanged'
 Node = namedtuple('Node', 'name value status')
 
 
-def get_difference(first_file, second_file):  # noqa: WPS210
-    common_keys = first_file.keys() & second_file.keys()
-    added_keys = second_file.keys() - first_file.keys()
-    deleted_keys = first_file.keys() - second_file.keys()
+def get_difference(first_data, second_data):  # noqa: WPS210
+    common_keys = first_data.keys() & second_data.keys()
+    added_keys = second_data.keys() - first_data.keys()
+    deleted_keys = first_data.keys() - second_data.keys()
     diff = []
 
     for key in common_keys:
-        add_common_keys_node(diff, key, first_file, second_file)
+        add_common_keys_node(diff, key, first_data, second_data)
 
     for added_key in added_keys:
-        add_node(diff, added_key, second_file, ADDED)
+        add_node(diff, added_key, second_data, ADDED)
 
     for deleted_key in deleted_keys:
-        add_node(diff, deleted_key, first_file, DELETED)
+        add_node(diff, deleted_key, first_data, DELETED)
 
     return diff
 
 
-def add_node(tree, name, input_file, node_status):
-    tree.append(Node(name, input_file[name], node_status))
+def add_node(tree, name, input_data, node_status):
+    tree.append(Node(name, input_data[name], node_status))
 
 
-def add_common_keys_node(tree, name, previous, current):
-    previous_value = previous[name]
-    current_value = current[name]
+def add_common_keys_node(tree, name, previous_data, current_data):
+    previous_value = previous_data[name]
+    current_value = current_data[name]
     if isinstance(previous_value, dict) and isinstance(current_value, dict):
         node_value = get_difference(previous_value, current_value)
         tree.append(Node(name, node_value, PARENT))
